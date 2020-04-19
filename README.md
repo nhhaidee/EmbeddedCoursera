ada8abdbac9e1529827e0eae0a434a33d81027cb
HaiNguyen <nhhaidee@gmail.com>
2020-04-19 15:07:49 -0600

    Implement functions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-/* Add Author and Project Details here */
+Author: Hai Nguyen
+Project Details:
+
+This is the first assignment of Embedded Specialization of University of Colorado, with given array the project will find statistics of array:
+    main() - The main entry point for your program
+    print_statistics() - A function that prints the statistics of an array including minimum, maximum, mean, and median.
+    print_array() - Given an array of data and a length, prints the array to the screen
+    find_median() - Given an array of data and a length, returns the median value
+    find_mean() - Given an array of data and a length, returns the mean
+    find_maximum() - Given an array of data and a length, returns the maximum
+    find_minimum() - Given an array of data and a length, returns the minimum
+    sort_array() - Given an array of data and a length, sorts the array from largest to smallest. (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
--- stats.c
+++ stats.c
@@ -32,40 +32,91 @@
 
 int compare(const void * a, const void * b) 
 {
- 
+   return (*(unsigned char*)b - *(unsigned char*)a);
 }
 
 void sort_array(unsigned char * ptr)
 {
-
+  int i;
+  qsort(ptr, SIZE, sizeof(unsigned char), compare);
+  printf("Sorted Array (Descending):");
+  for(i = 0 ; i < SIZE; i++ ) 
+  {   
+    printf("%u ", *ptr);
+    ptr++;
+  }
+  printf("\n");
 }
 
 float find_median(unsigned char * ptr)
 {
-
+  sort_array(ptr);
+  if (SIZE % 2 != 0)
+    return (float)ptr[SIZE/2];
+  else
+    return (float)(ptr[(SIZE-1)/2] + ptr[SIZE/2])/2.0;
 }
 void print_array(unsigned char * ptr)
 {
-
+  int i;
+  printf("Unsorted Array:");
+  for (i = 0; i < SIZE ; i++)
+  {
+    printf("%u ", *ptr);
+    ptr++;
+  }
+  printf("\n");
 }
 
 void print_statistics(unsigned char * ptr)
 {
-
+  printf("********Array Statistics***********\n");
+  printf("Minimum value of array: %u\n", find_minimum(ptr));
+  printf("Maximum value of array: %u\n", find_maximum(ptr));
+  printf("Mean value of array: %0.2f\n", find_mean(ptr));
+  printf("Median value of array: %0.2f\n", find_median(ptr));
 }
 
 float find_mean(unsigned char * ptr)
 {
+  int i;
+  int sum = 0;
+  for (i = 0; i < SIZE ; i++)
+  {
+    sum += *ptr;
+    ptr++;
+  }
 
+  return (float)(sum/SIZE);
 }
 
 unsigned char find_minimum (unsigned char * ptr)
 {
-
+  int i;
+  unsigned char min_value  = *ptr;
+  for (i = 0; i < SIZE ; i++)
+  {
+    if (min_value >= *ptr)
+    {
+      min_value = *ptr;
+    }
+    ptr++;
+  }
+  return min_value;
 }
 unsigned char find_maximum (unsigned char * ptr)
 {
-
+  int i;
+  unsigned char max_value  = *ptr;
+  for (i = 0; i < SIZE ; i++)
+  {
+    if (max_value <= *ptr)
+    {
+      max_value = *ptr;
+    }
+    ptr++;
+  }
+  return max_value;
 }
 
 void main() {
@@ -76,7 +127,8 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-
+  print_array(test); // Print original array
+  print_statistics(test); // Print statistic of array inluding sort array in descending order
 }
 
 
