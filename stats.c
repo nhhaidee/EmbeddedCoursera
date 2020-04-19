02a99bd71c894355ee9fdfc85127e098078f69f4
HaiNguyen <nhhaidee@gmail.com>
2020-04-19 15:03:11 -0600

    Adding code outline and documentation for C1M1 Assignment

--- stats.c
+++ stats.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
+ * Modidy and add more code by Hai Nguyen (Coursera)
  * Redistribution, modification or use of this software in source or binary
  * forms is permitted as long as the files maintain this copyright. Users are 
  * permitted to modify this and use it to learn about the field of embedded
@@ -23,21 +23,51 @@
 
 #include <stdio.h>
 #include "stats.h"
+#include "stdlib.h"
+
 
 /* Size of the Data Set */
 #define SIZE (40)
 
-void print_array(unsinged char * ptr)
+
+int compare(const void * a, const void * b) 
 {
-  int i;
-  printf("Array:\n");
-  for (i = 0; i < SIZE ; i++)
-  {
-    printf("%d ", *ptr);
-    ptr++;
-  }
-  printf("\n");
+ 
 }
+
+void sort_array(unsigned char * ptr)
+{
+
+}
+
+float find_median(unsigned char * ptr)
+{
+
+}
+void print_array(unsigned char * ptr)
+{
+
+}
+
+void print_statistics(unsigned char * ptr)
+{
+
+}
+
+float find_mean(unsigned char * ptr)
+{
+
+}
+
+unsigned char find_minimum (unsigned char * ptr)
+{
+
+}
+unsigned char find_maximum (unsigned char * ptr)
+{
+
+}
+
 void main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
@@ -46,9 +76,7 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-  print_array(test);
+
 }
 
-/* Add other Implementation File Code Here */
+
--- stats.h
+++ stats.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
+ * Modidy and add more code by Hai Nguyen (Coursera)
  * Redistribution, modification or use of this software in source or binary
  * forms is permitted as long as the files maintain this copyright. Users are 
  * permitted to modify this and use it to learn about the field of embedded
@@ -21,20 +21,62 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @brief <Function to print statistics of array (max, min, mean, median)>
+ * @param:
+ *    int * ptr: Pointer to a data set
+ * @return Print max, min, mean, median of array
  */
+void print_statistics(unsigned char * ptr);
 
+/**
+ * @brief <Function to print elements of array>
+ * @param:
+ *    int * ptr: Pointer to a data set
+ * @return print elements of array
+ */
+void print_array(unsigned char * ptr);
+
+/**
+ * @brief <Function to find median of array>
+ * @param:
+ *    int * ptr: Pointer to a data set
+ * @return median of array
+ */
+float find_median(unsigned char * ptr);
+
+/**
+ * @brief <Function to find mean of array>
+ * @param:
+ *    int * ptr: Pointer to a data set
+ * @return mean of array
+ */
+float find_mean(unsigned char * ptr);
+
+/**
+ * @brief <Function to find minimum of array>
+ * @param:
+ *    int * ptr: Pointer to a data set
+ * @return minimum of array
+ */
+unsigned char find_minimum (unsigned char * ptr);
+
+/**
+ * @brief <Function to find maximum of array>
+ * @param:
+ *    int * ptr: Pointer to a data set
+ * @return maximum of array
+ */
+unsigned char find_maximum (unsigned char * ptr);
+
+/**
+ * @brief <Functions to sort array in descending order>
+ * @param:
+ *    int * ptr: Pointer to a data set
+ * @return sorted array in descending order
+ */
+int compare (const void * a, const void * b);
+void sort_array(unsigned char * ptr);
 
 #endif /* __STATS_H__ */
